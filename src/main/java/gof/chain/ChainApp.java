a06656d8d9daf7c81a3fcb1afcb60a1ae3f09324
marie <mariemiroshnychenko@gmail.com>
2019-04-29 22:12:47 +0300

    Chain of responsibility pattern implementation added

--- src/main/java/gof/chain/ChainApp.java
+++ src/main/java/gof/chain/ChainApp.java
@@ -5,17 +5,17 @@ import gof.chain.entity.FileLogger;
 import gof.chain.entity.Logger;
 import gof.chain.entity.SMSLogger;
 
-public class ChainApp {
+public class ChainApp implements LevelConstant{
     public static void main(String[] args) {
-        Logger smsLogger = new SMSLogger(LevelConstant.ERROR);
-        Logger fileLogger = new FileLogger(LevelConstant.DEBUG);
-        Logger emailLogger = new EmailLogger(LevelConstant.INFO);
+        Logger smsLogger = new SMSLogger(ERROR);
+        Logger fileLogger = new FileLogger(DEBUG);
+        Logger emailLogger = new EmailLogger(INFO);
 
         smsLogger.setNextLogger(fileLogger);
         fileLogger.setNextLogger(emailLogger);
 
-        smsLogger.writeMessage("System is down", LevelConstant.ERROR);
-        smsLogger.writeMessage("The debug process is running", LevelConstant.DEBUG);
-        smsLogger.writeMessage("All right", LevelConstant.INFO);
+        smsLogger.writeMessage("System is down", ERROR);
+        smsLogger.writeMessage("The debug process is running", DEBUG);
+        smsLogger.writeMessage("All right", INFO);
     }
 }
--- src/main/java/gof/chain/LevelConstant.java
+++ src/main/java/gof/chain/LevelConstant.java
@@ -1,7 +1,7 @@
 package gof.chain;
 
-public class LevelConstant {
-    public static final int ERROR = 1;
-    public static final int DEBUG = 2;
-    public static final int INFO = 3;
+public interface LevelConstant {
+    int ERROR = 1;
+    int DEBUG = 2;
+    int INFO = 3;
 }
