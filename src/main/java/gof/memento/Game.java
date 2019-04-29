a42eeec481c53cba5fc86dcf30cd7b76a294747e
marie <mariemiroshnychenko@gmail.com>
2019-04-29 16:17:06 +0300

    Added to Game opportunity to load and save game

--- src/main/java/gof/memento/Game.java
+++ src/main/java/gof/memento/Game.java
@@ -8,4 +8,21 @@ public class Game {
         this.level = level;
         this.ms = ms;
     }
+
+    public void load(Save save) {
+        level = save.getLevel();
+        ms = save.getMs();
+    }
+
+    public Save save() {
+        return new Save(level, ms);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "level='" + level + '\'' +
+                ", ms=" + ms +
+                '}';
+    }
 }
