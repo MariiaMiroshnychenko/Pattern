a5488ebafe61bec1cb1ceebe77641e6a80bf3825
marie <mariemiroshnychenko@gmail.com>
2019-04-29 16:08:12 +0300

    Created class File that stored Save`s instance, setter and getter

--- src/main/java/gof/memento/File.java
+++ src/main/java/gof/memento/File.java
@@ -1,4 +1,13 @@
 package gof.memento;
 
 public class File {
+    private Save save;
+
+    public Save getSave() {
+        return save;
+    }
+
+    public void setSave(Save save) {
+        this.save = save;
+    }
 }
